d52c0712a16b596ba3573d1504657c7b4b580863
nataliacvillanueva <85206532+nataliacvillanueva@users.noreply.github.com>
2025-02-26 22:24:09 +0000

    Deploying to gh-pages from @ nataliacvillanueva/nataliacvillanueva.github.io@be6b8003344ebb8c765781b74b5f8e23093782b3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,14 +492,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6E%61%74%61%6C%69%61%76%69%6C%6C%61%6E%75%65%76%61@%75%74%65%78%61%73.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/nataliacvillanueva", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/natalia-villanueva-060041203", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0001-6917-4656", "_blank");
         },
       },{
         id: 'social-rss',
@@ -509,18 +523,11 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/nataliav_astro", "_blank");
         },
       },{
       id: 'light-theme',
