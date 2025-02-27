2e5ede84ff58b981f78b9fb6caf46cbd479fe6e4
nataliacvillanueva <85206532+nataliacvillanueva@users.noreply.github.com>
2025-02-27 16:46:46 +0000

    Deploying to gh-pages from @ nataliacvillanueva/nataliacvillanueva.github.io@8d3d62a76fb0ebbb1fef308fe34be6f7977a7a0f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,11 +24,11 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "CV",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/assets/pdf/NataliaVillanueva_CV_92024.pdf";
           },
         },{id: "post-a-post-with-image-galleries",
       
