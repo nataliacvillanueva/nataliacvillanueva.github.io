fb1390602ddbdc6f11dbccbd883a2922f364084f
nataliacvillanueva <85206532+nataliacvillanueva@users.noreply.github.com>
2025-06-16 01:19:13 +0000

    Deploying to gh-pages from @ nataliacvillanueva/nataliacvillanueva.github.io@020d6e75ca4ea25b3d96ea31a9008662f711fb77 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -393,9 +393,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-size-growth-of-star-formation",
+          title: 'Size growth of star formation',
+          description: "Measuring multiwavelength galaxy sizes at z~4-5",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
