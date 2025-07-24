9af5befa22a3bdfa0b8b91ec90c97bd219f6b2f5
nataliacvillanueva <85206532+nataliacvillanueva@users.noreply.github.com>
2025-07-24 21:29:26 +0000

    Deploying to gh-pages from @ nataliacvillanueva/nataliacvillanueva.github.io@b87957d29d5c23f508f9e78a7fcc9a8ee0eeb0b1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -408,11 +408,6 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
             },},{
         id: 'social-email',
         title: 'email',
